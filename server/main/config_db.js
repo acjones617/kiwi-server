d4c7017f6021bdc7f7668e9ee1316664bd6842b2
Andrew Jones <acjones617@gmail.com>
2014-07-11 17:34:40 -0700

    Set up db production config - using git ignored development config for now

--- server/main/config_db.js
+++ server/main/config_db.js
@@ -1,20 +1,13 @@
+var config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
 
-module.exports = function (app){
-  var config;
-
-  if (app.get('ENV') === 'development') {
-    config = require('config_db_development');
-  } else {
-    config = {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      server: process.env.DB_SERVER,
-      database: process.env.DB_DATABASE,
-
-      options: {
-        encrypt: true // Use this if you're on Windows Azure
-      }
-
-    }
+  options: {
+    encrypt: true // Use this if you're on Windows Azure
   }
+
 }
+
+module.exports = config;
