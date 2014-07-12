0d655a31b20412a76b186468566d2c158a473974
Andrew Jones <acjones617@gmail.com>
2014-07-11 17:16:55 -0700

    Clean up config file. No longer using mongo

--- server/main/config.js
+++ server/main/config.js
@@ -2,57 +2,27 @@
 
 var bodyParser     = require('body-parser'),
     middle         = require('./middleware'),
-    mongoose       = require('mongoose'),
     morgan         = require('morgan'),
     methodOverride = require('method-override'),
-    cookieParser   = require('cookie-parser'),
-    session        = require('express-session'),
-    mongoStore     = require('connect-mongo')(session),
-    cors = require('cors'),
+    cors           = require('cors'),
     Promise        = require('bluebird');
 
-    // compression = require('compression'),
-    // favicon = require('static-favicon'),
-    // path = require('path'),
-    // config = require('./config'),
-    // errorHandler = require('errorhandler'),
 /*
  * Include all global env variables here.
 */
 module.exports = exports = function (app, express, routers) {
-  // var env = app.get('env');
+  app.set('ENV', 'development');
+
+  var db_config = require('config_db')(app);
 
   var db_url = process.env.DB_URL || 'mongodb://localhost/kiwiApp';
   app.set('DB_URL', db_url);
 
-  // connect to MongoDB
-  mongoose.connect(app.get('DB_URL'));
+  // connect to SQL
+
 
   // Bootstrap and promisify models
   var User = require('../user/user-model');
-
-  Promise.promisifyAll(User);
-  Promise.promisifyAll(mongoose);
-
-  // Passport Configuration
-  var passport = require('../session/passport')
-    
-  // if ('development' === env) {
-  //   app.use(require('connect-livereload')());
-
-
-  //   app.use(express.static(path.join(config.root, '.tmp')));
-  //   app.use(express.static(path.join(config.root, 'app')));
-  //   app.set('views', config.root + '/app/views');
-  // }
-
-  // if ('production' === env) {
-  //   app.use(compression());
-  //   app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
-  //   app.use(express.static(path.join(config.root, 'public')));
-  //   app.set('views', config.root + '/views');
-  // }
-
 
   app.engine('html', require('ejs').renderFile);
   app.set('view engine', 'html');
