de75ebab54f3ff0b19ac54c6ec370e1b0898d30c
Andrew Jones <acjones617@gmail.com>
2014-07-22 11:46:37 -0700

    Correctly creates user table

--- server/schema/schema-controller.js
+++ server/schema/schema-controller.js
@@ -3,19 +3,27 @@ var config = require('../main/config_db_development');
 var Promise = require('bluebird');
 var query = require('./schema-queries');
 
-var connection = new sql.Connection(config, function(err) {
-    if (err) {
-      console.log('API database connection error:');
-      console.log(err);
-      process.exit();
-    } else {
-      console.log('API database connection established.');
-      var request = new sql.Request(connection);
-      Promise.promisify(request.query);
+module.exports = {
+  rebuild: function(req, res) {
+    var connection = new sql.Connection(config, function(err) {
+      if (err) {
+        console.log('API database connection error:');
+        console.log(err);
+        process.exit();
+      } else {
+        console.log('API database connection established.');
+        var request = new sql.Request(connection);
+        Promise.promisifyAll(request);
 
-      request.queryAsync(query.createUsers)
-      .then(function() {
-        return request.queryAsync(query.createKiwis)
-      });
-    }
-});
+        request.queryAsync(query.createUsers)
+        .then(function(result) {
+          console.log('success!!!', result);
+          res.end(200);
+        })
+        .catch(function(err) {
+          console.log('error!!!!', err);
+        });
+      }
+    })
+  }
+};
