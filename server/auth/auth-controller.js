eb9d9eacac444e9d79ab807aaa6dc2ac3e1b2b48
Andrew Jones <acjones617@gmail.com>
2014-07-23 11:59:07 -0700

    Add login function

--- server/auth/auth-controller.js
+++ server/auth/auth-controller.js
@@ -1,9 +1,11 @@
 'use strict';
 
-// var mongoose = require('mongoose'),
 var passport = require('passport');
 var jwt = require('jsonwebtoken');
-var User = mongoose.model('User');
+var bcrypt = require('bcrypt');
+var Promise = require('bluebird');
+
+Promise.promisifyAll(bcrypt);
 
 module.exports = {
 
@@ -19,24 +21,21 @@ module.exports = {
     passport.authenticate('local', function(err, user, info) {
       var error = err || info;
       if (error) return res.json(401, error);
+      
       // user has authenticated correctly - provide user with JWT token, expires in one year, send back username
-      User.findOneAsync({email: req.body.email})
-      .then(function(foundUser) {
-        var tokenSecret = process.env.SECRET_JWT || 'secret'
-        var token = jwt.sign({ username: req.body.name }, tokenSecret, { expiresInMinutes: 60 * 24 * 365 });
-        res.json({
-          token: token,
-          name: foundUser.name
-        });
-      })
-      .catch(function(err) {
-        console.log(err);
-        res.send(err);
-      })
+      var tokenSecret = process.env.SECRET_JWT || 'super secret token'
+      // expires in one year?
+      var token = jwt.sign({ email: req.body.email }, tokenSecret, { expiresInMinutes: 60 * 24 * 365 });
+      res.json({ 'kiwi-token': token });
     })(req, res, next);
   },
 
 
+// bcrypt.genSaltAsync(10)
+//           .then(function(salt) {
+//             return bcrypt.hashAsync(password, salt);
+//           })
+//           .then()
   create: function (req, res, next) {
     console.log('inside create');
     var newUser = new User(req.body);
