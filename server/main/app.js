fc2886d5d3650a89b4c62dd189558e16b408b4d6
Andrew Jones <acjones617@gmail.com>
2014-07-22 22:56:14 -0700

    Add auth routing

--- server/main/app.js
+++ server/main/app.js
@@ -4,21 +4,20 @@ var express      = require('express');
 var app          = express();
 
 /* Router */
-var UserRouter        = express.Router();
-var SessionRouter     = express.Router();
 var SchemaRouter      = express.Router();
+var LoginRouter       = express.Router();
+var AuthRouter        = express.Router();
 
 var routers = {};
 
-routers.UserRouter        = UserRouter;
-routers.SessionRouter     = SessionRouter;
 routers.SchemaRouter      = SchemaRouter;
+routers.AuthRouter        = AuthRouter;
+// routers.UserRouter        = UserRouter;
 
 require('./config.js')(app, express, routers);
 
-// require('../user/user-routes')(UserRouter);
-// require('../session/session-routes')(SessionRouter);
 require('../schema/schema-routes')(SchemaRouter);
+require('../auth/auth-routes')(SessionRouter);
 
 module.exports = exports = app;
 
