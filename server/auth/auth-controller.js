13ed8e139c6ac5143e2626884ad7c9c5468a1ccc
Andrew Jones <acjones617@gmail.com>
2014-07-23 14:05:44 -0700

    Pull jwt creation into own function, write signup function

--- server/auth/auth-controller.js
+++ server/auth/auth-controller.js
@@ -3,9 +3,18 @@
 var passport = require('passport');
 var jwt = require('jsonwebtoken');
 var bcrypt = require('bcrypt');
+var config = require('../main/config_db_development');
 var Promise = require('bluebird');
+var query = require('./auth-queries');
 
 Promise.promisifyAll(bcrypt);
+
+var createJwt = function(req, res) {
+  var tokenSecret = process.env.SECRET_JWT || 'super secret token'
+  // expires in one year?
+  var token = jwt.sign({ email: req.body.email }, tokenSecret, { expiresInMinutes: 60 * 24 * 365 });
+  res.json({ 'kiwi-token': token });
+}
 
 module.exports = {
 
@@ -21,61 +30,39 @@ module.exports = {
     passport.authenticate('local', function(err, user, info) {
       var error = err || info;
       if (error) return res.json(401, error);
-      
       // user has authenticated correctly - provide user with JWT token, expires in one year, send back username
-      var tokenSecret = process.env.SECRET_JWT || 'super secret token'
-      // expires in one year?
-      var token = jwt.sign({ email: req.body.email }, tokenSecret, { expiresInMinutes: 60 * 24 * 365 });
-      res.json({ 'kiwi-token': token });
+      createJwt(req, res);
+      res.end('success!');
     })(req, res, next);
   },
 
-
-// bcrypt.genSaltAsync(10)
-//           .then(function(salt) {
-//             return bcrypt.hashAsync(password, salt);
-//           })
-//           .then()
   create: function (req, res, next) {
     console.log('inside create');
-    var newUser = new User(req.body);
-    newUser.provider = 'local';
-    newUser.save(function(err, user) {
-      if (err) {
-        console.log('ERROR', err);
-        return res.json(400, err);
-      }
+    bcrypt.genSaltAsync(10)
+    .then(function(salt) {
+      return bcrypt.hashAsync(req.body.password, salt);
+    })
+    .then(function(hash_password) {
+      // insert into sql
+      var connection = new sql.Connection(config, function(err) {
+        if (err) {
+          console.log('API database connection error:');
+          console.log(err);
+        } else {
+          var request = new sql.Request(connection);
+          Promise.promisifyAll(request);
 
-      /**
-       * Create new player with random mapId
-       */
-      console.log(req.body);
-      // update player with new mapId
-      return Screen.findAsync()
-      .then(function(screens) {
-        var mapId = screens[0]._id;
-        var newPlayer = new Player({
-          user: user._id,
-          username: req.body.name,
-          mapId: mapId
-        });
-
-        newPlayer.save(function(err, result) {
-          if (err) {
-            console.log('ERROR with player', err);
-            return res.json(400, err);
-          }
-
-          var tokenSecret = process.env.SECRET_JWT || 'secret'
-          var token = jwt.sign({ username: req.body.name }, tokenSecret, { expiresInMinutes: 60 * 24 * 365 });
-          console.log(req.body.name);
-          return res.json({
-            token: token,
-            name: req.body.name
-          });
-        });
+          request.queryAsync(query.signupUser(email, hash_password))
+          .then(function() {
+            // send back jwt
+            createJwt(req, res);
+            res.end('success!');
+          })
+          .catch(function(err) {
+            res.send(500);
+          })
+        }
       });
-      
     });
-  },
+  }
 };
