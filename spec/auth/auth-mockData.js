d1b05b8f04eb0fed3cb23399f942ee8dc01e830b
Andrew Jones <acjones617@gmail.com>
2014-07-23 13:50:50 -0700

    Add mock login data for testing purposes

---
+++ spec/auth/auth-mockData.js
@@ -0,0 +1,18 @@
+module.exports = exports = data = {};
+
+data.signup = {
+  email:          'bobsmith@gmail.com',
+  password:       'verysecure password'
+};
+
+data.login = {};
+
+data.login.valid = {
+  email:          'bobsmith@gmail.com',
+  password:       'verysecure password'
+};
+
+data.login.invalid = {
+  email:          'bobsmith@gmail.com',
+  password:       'incorrect password'
+};
