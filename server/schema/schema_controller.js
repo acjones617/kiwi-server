2e589fd3ded81d09791d6189fc6d9b660e5dc82f
Andrew Jones <acjones617@gmail.com>
2014-07-26 14:18:55 -0700

    Add dummy data, return dummy data at end of request

--- server/schema/schema_controller.js
+++ server/schema/schema_controller.js
@@ -45,24 +45,32 @@ module.exports = function(app) {
         request(app)
         .post('/auth/signup')
         .send({ email: 'test@test.com',password: 'verysecure password' })
-        .end(function (err, res) {
+        .end(function (err, response) {
           // sample user created, create sample kiwi
           console.log('dummy user created!!');
           dbRequest.queryAsync(query.insertKiwi)
           .then(function() {
             console.log('insert seed kiwi success!!!');
-            return dbRequest.queryAsync(query.insertKiwiValue)
+            return dbRequest.queryAsync(query.insertKiwiValue);
           })
           .then(function() {
             console.log('insert seed kiwi value success!!!');
-            return dbRequest.queryAsync(query.insertKiwiGroup)
+            return dbRequest.queryAsync(query.insertGroup);
+          })
+          .then(function() {
+            console.log('insert seed group success!!!');
+            return dbRequest.queryAsync(query.insertKiwiGroup);
           })
           .then(function() {
             console.log('added seed kiwi group!!!')
+            return dbRequest.queryAsync(query.selectAll);
             // donezo
-            res.end('success!');
           })
-          .catch(function() {
+          .then(function(foundAll) {
+            console.log('FOUND DUMMY DATA!!', foundAll);
+            res.send(foundAll);
+          })
+          .catch(function(err) {
             console.log('error!!!!', err);
             res.send('error!');
           });
