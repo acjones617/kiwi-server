113f83eb4f2ea50371a325c816879be0aa5d687a
Andrew Jones <acjones617@gmail.com>
2014-07-26 17:18:42 -0700

    Write tests for kiwi endpoints. All pass

--- spec/kiwi/kiwi_controller_spec.js
+++ spec/kiwi/kiwi_controller_spec.js
@@ -37,34 +37,96 @@ describe('Kiwi Controller', function () {
 
 describe('Kiwi Controller', function () {
 
-  // it('should be able to add a new kiwi', function (done) {
-  //   // delete kiwi if it exists
-  //   dbRequest.queryAsync(mockData.queries.deleteKiwi)
-  //   .then(function() {
-  //     request(app)
-  //     .post('/api/kiwi/add')
-  //     .set('x-access-token', authData.validJwt)
-  //     .send(mockData.kiwi.valid)
-  //     .end(function (err, res) {
-  //       if (err) return done(err);
-  //       expect(res.statusCode).toEqual(201);
-  //       expect(res.body.message).toEqual('kiwi created');
-  //       done();
-  //     });
-  //   })
-  // });
+  it('should be able to add a new kiwi', function (done) {
+    // delete all kiwi values on kiwi if they exist
+    dbRequest.queryAsync(mockData.queries.deleteKiwiValues)
+    .then(function() {
+      // delete kiwi if it exists
+      return dbRequest.queryAsync(mockData.queries.deleteKiwi)
+    })
+    .then(function() {
+      request(app)
+      .post('/api/kiwi/add')
+      .set('x-access-token', authData.validJwt)
+      .send(mockData.kiwi.valid)
+      .end(function (err, res) {
+        if (err) return done(err);
+        console.log('CREATED KIWI?!', res.body.message);
+        expect(res.statusCode).toEqual(201);
+        expect(res.body.message).toEqual('kiwi created');
+        done();
+      });
+    })
+  });
 
-  // it('should not be able to add an already existing kiwi', function (done) {
-  //   request(app)
-  //   .post('/api/kiwi/add')
-  //   .set('x-access-token', authData.validJwt)
-  //   .send(mockData.kiwi.valid)
-  //   .end(function (err, res) {
-  //     if (err) return done(err);
-  //     expect(res.statusCode).toEqual(403);
-  //     expect(res.body.error).toEqual('kiwi already exists');
-  //     done();
-  //   });
-  // });
+  it('should not be able to add an already existing kiwi', function (done) {
+    request(app)
+    .post('/api/kiwi/add')
+    .set('x-access-token', authData.validJwt)
+    .send(mockData.kiwi.invalid)
+    .end(function (err, res) {
+      if (err) return done(err);
+      expect(res.statusCode).toEqual(403);
+      expect(res.body.error).toEqual('kiwi already exists');
+      done();
+    });
+  });
 
+
+  it('should be able to add a kiwi value', function (done) {
+    getTestKiwi()
+    .then(function(foundKiwi) {
+      request(app)
+      .post('/api/kiwi/values/' + foundKiwi[0].id)
+      .set('x-access-token', authData.validJwt)
+      .send(mockData.kiwiValues)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(201);
+        done();
+      });
+    })
+  });
+
+
+  it('should be able to retrieve that same kiwi value', function (done) {
+    getTestKiwi()
+    .then(function(foundKiwi) {
+      request(app)
+      .get('/api/kiwi/values/' + foundKiwi[0].id)
+      .set('x-access-token', authData.validJwt)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.data[0].value).toEqual(mockData.kiwiValues.kiwiData.value);
+        done();
+      });
+    })
+  });
+
+  it('should be able to retrieve kiwis needing updating', function (done) {
+    request(app)
+    .get('/api/kiwi/update')
+    .set('x-access-token', authData.validJwt)
+    .end(function (err, res) {
+      if (err) return done(err);
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.kiwisNeedingUpdate[0].title).toEqual(testData.kiwi.kiwiData.title);
+      done();
+    });
+  });
+
+  it('should be able to remove an already existing kiwi', function (done) {
+    getTestKiwi()
+    .then(function(foundKiwi) {
+      request(app)
+      .put('/api/kiwi/remove/' + foundKiwi[0].id)
+      .set('x-access-token', authData.validJwt)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(200);
+        done();
+      });
+    })
+  });
 });
