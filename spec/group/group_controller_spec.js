10c5e732b33723c045e19d2c97ebc0d7b19791ea
Andrew Jones <acjones617@gmail.com>
2014-07-28 17:21:36 -0700

    Test group endpoints

--- spec/group/group_controller_spec.js
+++ spec/group/group_controller_spec.js
@@ -6,9 +6,14 @@ var groupCtrl = require('../../server/group/group_controller');
 var mockData = require('./group_mockData');
 var authData = require('../../server/main/config_db_development');
 var testData = require('../../server/schema/schema_mockData');
+var kiwiData = require('../kiwi/kiwi_mockData');
 
 var getTestGroup = function() {
   return dbRequest.queryAsync(mockData.queries.getTestGroup);
+}
+
+var getNewGroup = function() {
+  return dbRequest.queryAsync(mockData.queries.getNewGroup);
 }
 
 describe('Group Controller', function () {
@@ -33,37 +38,102 @@ describe('Group Controller', function () {
 
 describe('Group Controller', function () {
 
-  // it('should return group info', function (done) {
-  //   getTestGroup()
-  //   .then(function(foundGroup) {
-  //     request(app)
-  //     .get('/api/group/info/' + foundGroup[0].id)
-  //     .set('x-access-token', authData.validJwt)
-  //     .end(function (err, res) {
-  //       if (err) return done(err);
-  //       expect(res.statusCode).toEqual(200);
-  //       expect(res.body.group.name).toEqual(testData.group.name);
-  //       done();
-  //     });
-  //   });
-  // });
+  it('should return group info', function (done) {
+    getTestGroup()
+    .then(function(foundGroup) {
+      request(app)
+      .get('/api/group/info/' + foundGroup[0].id)
+      .set('x-access-token', authData.validJwt)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.group.name).toEqual(testData.group.groupData.name);
+        done();
+      });
+    });
+  });
 
-  // it('should return the group\'s kiwis', function (done) {
-  //   getTestGroup()
-  //   .then(function(foundGroup) {
-  //     request(app)
-  //     .get('/api/group/kiwis/' + foundGroup[0].id)
-  //     .set('x-access-token', authData.validJwt)
-  //     .end(function(err, res) {
-  //       if (err) return done(err);
-  //       console.log(res.body);
-  //       expect(res.statusCode).toEqual(200);
-  //       expect(res.body.kiwis[0].title).toEqual(testData.kiwi.title);
-  //       done();
-  //     })
-  //   })
-  // });
+  it('should return the group\'s kiwis', function (done) {
+    getTestGroup()
+    .then(function(foundGroup) {
+      request(app)
+      .get('/api/group/kiwis/' + foundGroup[0].id)
+      .set('x-access-token', authData.validJwt)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.kiwis[0].title).toEqual(testData.kiwi.kiwiData.title);
+        done();
+      })
+    })
+  });
 
+  it('should create a new group', function (done) {
+    // delete all kiwi group combos on kiwi_group if they exist
+    dbRequest.queryAsync(mockData.queries.deleteKiwiGroups)
+    .then(function() {
+      // delete group if it exists
+      return dbRequest.queryAsync(mockData.queries.deleteGroup)
+    })
+    .then(function() {
+      request(app)
+      .post('/api/group/create')
+      .set('x-access-token', authData.validJwt)
+      .send(mockData.group.valid)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(201);
+        done();
+      })
+    });
+  });
 
+  it('should not allow user to create groups with same name', function (done) {
+    request(app)
+    .post('/api/group/create')
+    .set('x-access-token', authData.validJwt)
+    .send(mockData.group.invalid)
+    .end(function(err, res) {
+      if (err) return done(err);
+      expect(res.statusCode).toEqual(403);
+      expect(res.body.error).toEqual('user already has group by that name');
+      done();
+    });
+  });
+
+  it('should add kiwi to group', function (done) {
+    var groupData;
+    getNewGroup()
+    .then(function(foundGroup) {
+      groupData = foundGroup;
+      return dbRequest.queryAsync(kiwiData.queries.getTestKiwi)
+    })
+    .then(function(kiwiData) {
+      request(app)
+      .post('/api/group/kiwis/' + groupData[0].id)
+      .set('x-access-token', authData.validJwt)
+      .send({ kiwiData: kiwiData[0] })
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(201);
+        done();
+      });
+    })
+  });
+
+  it('should pull appropriate kiwis from group', function (done) {
+    getNewGroup()
+    .then(function(groupData) {
+      request(app)
+      .get('/api/group/kiwis/' + groupData[0].id)
+      .set('x-access-token', authData.validJwt)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.kiwis[0].title).toEqual(kiwiData.kiwi.valid.kiwiData.title);
+        done();
+      });
+    })
+  });
 
 });
--- spec/group/group_mockData.js
+++ spec/group/group_mockData.js
@@ -6,10 +6,14 @@ module.exports = exports = data = {};
 
 data.group = {};
 
-data.group.valid = { 
-  name:         "Test Group 2",
-  description:  "Dummy group for tests"
+data.group.valid = {
+  groupData: {
+    name:         "Test Group 2",
+    description:  "Dummy group for tests"
+  }
 };
+
+data.group.invalid = testData.group;
 
 data.queries = {};
 
@@ -18,12 +22,31 @@ data.queries.getTestGroup =
   "FROM dbo.groups g\n" +
   "JOIN dbo.users u\n" +
   "ON g.user_id = u.id\n" +
-  "WHERE u.email = '" + testData.signup.email + "';";
+  "WHERE u.email = '" + testData.signup.email + "'\n" +
+  "AND g.name = '" + testData.group.groupData.name + "';";
 
-data.queries.deleteGroup =
-  "DELETE dbo.group\n" +
-  "FROM dbo.group g\n" +
+data.queries.getNewGroup = 
+  "SELECT g.*\n" +
+  "FROM dbo.groups g\n" +
   "JOIN dbo.users u\n" +
   "ON g.user_id = u.id\n" +
   "WHERE u.email = '" + testData.signup.email + "'\n" +
-  "AND g.name = " + testData.group.name;
+  "AND g.name = '" + data.group.valid.groupData.name + "';";
+
+data.queries.deleteGroup =
+  "DELETE dbo.groups\n" +
+  "FROM dbo.groups g\n" +
+  "JOIN dbo.users u\n" +
+  "ON g.user_id = u.id\n" +
+  "WHERE u.email = '" + testData.signup.email + "'\n" +
+  "AND g.name = '" + data.group.valid.groupData.name + "';";
+
+data.queries.deleteKiwiGroups =
+  "DELETE dbo.kiwi_group\n" +
+  "FROM dbo.kiwi_group kg\n" +
+  "JOIN dbo.groups g\n" +
+  "ON kg.group_id = g.id\n" +
+  "JOIN dbo.users u\n" +
+  "ON g.user_id = u.id\n" +
+  "WHERE u.email = '" + testData.signup.email + "'\n" +
+  "AND g.name = '" + data.group.valid.groupData.name + "';";
