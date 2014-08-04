d272fc12b755933a932b6e2fd6056ead6f153c0a
Andrew Jones <acjones617@gmail.com>
2014-08-04 16:41:44 -0700

    Add remove group and edit group info endpoints

--- server/group/group_controller.js
+++ server/group/group_controller.js
@@ -16,6 +16,16 @@ module.exports = {
     });
   },
 
+  editGroupInfo: function (req, res, next) {
+    dbRequest.queryAsync(query.editGroupInfo(req.user.email, req.params.groupId, req.body.groupData))
+    .then(function (foundGroup) {
+      res.send({ group: foundGroup[0] });
+    })
+    .catch(function (err) {
+      next({ error: err, status: 500 });
+    });
+  }
+
   getKiwis: function (req, res, next) {
     dbRequest.queryAsync(query.getKiwisOfGroup(req.params.groupId))
     .then(function (foundKiwis) {
@@ -35,6 +45,16 @@ module.exports = {
       next({ error: err, status: 500 });
     });
   },
+
+  removeKiwi: function(req, res, next) {
+    dbRequest.queryAsync(query.removeKiwiFromGroup(req.params.groupId, req.body.kiwiData))
+    .then(function() {
+      res.send(201);
+    })
+    .catch(function (err) {
+      next({ error: err, status: 500 });
+    })
+  }
 
   createGroup: function (req, res, next) {
     dbRequest.queryAsync(query.lookupGroup(req.user.email, req.body.groupData))
