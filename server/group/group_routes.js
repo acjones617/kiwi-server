ec52c293391fafa848ab1da7c75920ce02225a3a
Andrew Jones <acjones617@gmail.com>
2014-08-04 16:42:02 -0700

    Add remove group and edit group info routes

--- server/group/group_routes.js
+++ server/group/group_routes.js
@@ -4,11 +4,13 @@ var controller = require('./group_controller');
 
 module.exports = function(router) {
   router.route('/info/:groupId')
-    .get(controller.getGroupInfo);
+    .get(controller.getGroupInfo)
+    .put(controller.editGroupInfo);
 
   router.route('/kiwis/:groupId')
     .get(controller.getKiwis)
-    .post(controller.addKiwi);
+    .post(controller.addKiwi)
+    .delete(controller.removeKiwi);
 
   router.route('/create')
     .post(controller.createGroup);
